3b41b7da4e87043983248420f223d1d5a9469bef
goo3oo <goo3oo0918@gmail.com>
2025-02-12 21:49:44 +0900

    refactor: 유저 정보 수정 벨리데이션 추가

--- src/main/java/salute/oneshot/domain/user/dto/repuest/UserUpdateRequestDto.java
+++ src/main/java/salute/oneshot/domain/user/dto/repuest/UserUpdateRequestDto.java
@@ -1,13 +1,27 @@
 package salute.oneshot.domain.user.dto.repuest;
 
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import salute.oneshot.domain.user.dto.UserValidationMessage;
 
 @Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class UserUpdateRequestDto {
 
+    @Size(
+            min = UserValidationMessage.NICKNAME_MIN,
+            max = UserValidationMessage.NICKNAME_MAX,
+            message = UserValidationMessage.NICKNAME_RANGE_MESSAGE
+    )
     private final String nickName;
+
+    @Size(min = UserValidationMessage.PASSWORD_MIN, message = UserValidationMessage.PASSWORD_MIN_MESSAGE)
+    @Pattern(
+            regexp = UserValidationMessage.PASSWORD_REG,
+            message = UserValidationMessage.INVALID_PASSWORD_MESSAGE
+    )
     private final String password;
 }
