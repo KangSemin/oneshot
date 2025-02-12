5f7958aed74c710fc2384d76487c420b0d535f98
goo3oo <goo3oo0918@gmail.com>
2025-02-12 17:45:57 +0900

    feat: 로그인한 유저 정보 조회

--- src/main/java/salute/oneshot/domain/user/controller/UserController.java
+++ src/main/java/salute/oneshot/domain/user/controller/UserController.java
@@ -1,9 +1,17 @@
 package salute.oneshot.domain.user.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import salute.oneshot.domain.common.dto.success.ApiResponse;
+import salute.oneshot.domain.common.dto.success.ApiResponseMessage;
+import salute.oneshot.domain.user.dto.response.UserResponseDto;
 import salute.oneshot.domain.user.service.UserService;
+import salute.oneshot.global.security.entity.CustomUserDetails;
 
 @RestController
 @RequestMapping("/api/users")
@@ -11,4 +19,13 @@ import salute.oneshot.domain.user.service.UserService;
 public class UserController {
 
     private final UserService userService;
+
+    @GetMapping
+    public ResponseEntity<ApiResponse<UserResponseDto>> getUserInfo(
+            @AuthenticationPrincipal CustomUserDetails userDetails
+    ) {
+        UserResponseDto userResponseDto = userService.getUserInfo(userDetails.getId());
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(ApiResponse.success(ApiResponseMessage.GET_USER_SUCCESS, userResponseDto));
+    }
 }
--- src/main/java/salute/oneshot/domain/user/service/UserService.java
+++ src/main/java/salute/oneshot/domain/user/service/UserService.java
@@ -2,11 +2,20 @@ package salute.oneshot.domain.user.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import salute.oneshot.domain.common.dto.error.ErrorCode;
+import salute.oneshot.domain.user.dto.response.UserResponseDto;
+import salute.oneshot.domain.user.entity.User;
 import salute.oneshot.domain.user.repository.UserRepository;
+import salute.oneshot.global.exception.NotFoundException;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
 
+    public UserResponseDto getUserInfo(Long id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException(ErrorCode.USER_NOT_FOUND));
+        return UserResponseDto.from(user);
+    }
 }
