0fb09d93d116551a30e7aa1854dcfa97c9942e8b
cnux9 <cnux9@naver.com>
2025-02-12 17:48:37 +0900

    feat: 장바구니 수정 기능 추가(항목 제거, 수량 변경)

--- src/main/java/salute/oneshot/domain/cart/controller/CartController.java
+++ src/main/java/salute/oneshot/domain/cart/controller/CartController.java
@@ -1,12 +1,16 @@
 package salute.oneshot.domain.cart.controller;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import salute.oneshot.domain.cart.dto.request.AddCartItemRequestDto;
+import salute.oneshot.domain.cart.dto.request.UpdateCartItemAmountRequestDto;
 import salute.oneshot.domain.cart.dto.response.CartItemResponseDto;
 import salute.oneshot.domain.cart.dto.response.CartResponseDto;
 import salute.oneshot.domain.cart.dto.service.AddCartItemSDto;
+import salute.oneshot.domain.cart.dto.service.UpdateItemQuantitySDto;
 import salute.oneshot.domain.cart.service.CartService;
 import salute.oneshot.domain.common.dto.success.ApiResponse;
 import salute.oneshot.domain.common.dto.success.ApiResponseMessage;
@@ -21,12 +25,12 @@ public class CartController {
     @PostMapping("/items")
     public ResponseEntity<ApiResponse<CartItemResponseDto>> addItem(
             @RequestBody AddCartItemRequestDto requestDto
-//            @AuthenticationPrincipal Long userId
+//            @AuthenticationPrincipal CustomUserDetails userDetails
     ) {
 //        TODO: 시큐리티 설정 후 테스트용 데이터 지우기
         Long userId = 1L;
+        AddCartItemSDto sdto = AddCartItemSDto.of(userId, requestDto.getProductId(), requestDto.getQuantity());
 
-        AddCartItemSDto sdto = AddCartItemSDto.of(userId, requestDto.getProductId(), requestDto.getAmount());
         CartItemResponseDto responseDto = cartService.addCartItem(sdto);
 
         return ResponseEntity.ok(
@@ -37,7 +41,7 @@ public class CartController {
 
     @GetMapping
     public ResponseEntity<ApiResponse<CartResponseDto>> findCart(
-//            @AuthenticationPrincipal Long userId
+//            @AuthenticationPrincipal CustomUserDetails userDetails
     ) {
 //        TODO: 시큐리티 설정 후 테스트용 데이터 지우기
         Long userId = 1L;
@@ -49,9 +53,38 @@ public class CartController {
         );
     }
 
+    @PatchMapping("/items/{itemId}")
+    public ResponseEntity<ApiResponse<CartItemResponseDto>> updateItemQuantity(
+            @PathVariable Long itemId,
+            @RequestBody UpdateCartItemAmountRequestDto requestDto
+//            @AuthenticationPrincipal CustomUserDetails userDetails
+    ) {
+//        TODO: 시큐리티 설정 후 테스트용 데이터 지우기
+        Long userId = 1L;
+        UpdateItemQuantitySDto sdto = UpdateItemQuantitySDto.of(userId, itemId, requestDto) ;
+        CartItemResponseDto responseDto = cartService.updateItemQuantity(sdto);
+
+        return ResponseEntity.ok(
+                ApiResponse.success(ApiResponseMessage.UPDATE_CART_ITEM_QUANTITY_SUCCESS, responseDto)
+        );
+    }
+
+    @DeleteMapping("/items/{itemId}")
+    public ResponseEntity<ApiResponse<Void>> removeItem(
+            @PathVariable Long itemId
+//            @AuthenticationPrincipal CustomUserDetails userDetails
+    ) {
+//        TODO: 시큐리티 설정 후 테스트용 데이터 지우기
+        Long userId = 1L;
+        cartService.removeItem(userId, itemId);
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT)
+                .body(ApiResponse.success(ApiResponseMessage.REMOVE_CART_ITEM_SUCCESS));
+    }
+
     @DeleteMapping
     public ResponseEntity<ApiResponse<Void>> emptyCart(
-//            @AuthenticationPrincipal Long userId
+//            @AuthenticationPrincipal CustomUserDetails userDetails
     ) {
 //        TODO: 시큐리티 설정 후 테스트용 데이터 지우기
         Long userId = 1L;
