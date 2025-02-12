ee0c502d001215c5ce593aec9aed6d879012ade1
dornrrl0932 <dornrrl0932@naver.com>
2025-02-12 14:09:25 +0900

    fix: import 삭제

--- src/main/java/salute/oneshot/domain/common/dto/error/ErrorCode.java
+++ src/main/java/salute/oneshot/domain/common/dto/error/ErrorCode.java
@@ -3,7 +3,6 @@ package salute.oneshot.domain.common.dto.error;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import salute.oneshot.domain.recipe.entity.Recipe;
 
 @Getter
 @RequiredArgsConstructor
