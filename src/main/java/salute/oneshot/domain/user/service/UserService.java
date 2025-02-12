9e8dd11d6af260e8d8066c25ea881102ec25dace
goo3oo <goo3oo0918@gmail.com>
2025-02-12 17:50:35 +0900

    fix: 트랜잭셔널 리드온리 추가

--- src/main/java/salute/oneshot/domain/user/service/UserService.java
+++ src/main/java/salute/oneshot/domain/user/service/UserService.java
@@ -2,6 +2,7 @@ package salute.oneshot.domain.user.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import salute.oneshot.domain.common.dto.error.ErrorCode;
 import salute.oneshot.domain.user.dto.response.UserResponseDto;
 import salute.oneshot.domain.user.entity.User;
@@ -13,6 +14,7 @@ import salute.oneshot.global.exception.NotFoundException;
 public class UserService {
     private final UserRepository userRepository;
 
+    @Transactional(readOnly = true)
     public UserResponseDto getUserInfo(Long id) {
         User user = userRepository.findById(id)
                 .orElseThrow(() -> new NotFoundException(ErrorCode.USER_NOT_FOUND));
