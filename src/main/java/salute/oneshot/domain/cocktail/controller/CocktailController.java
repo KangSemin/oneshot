d7320e776d1ba1c62b37d690cd03fc838af2da9a
lee sujin <jin18302@naver.com>
2025-02-12 15:28:34 +0900

    feat: 칵테일 단건 조회 기능 추가

--- src/main/java/salute/oneshot/domain/cocktail/controller/CocktailController.java
+++ src/main/java/salute/oneshot/domain/cocktail/controller/CocktailController.java
@@ -6,10 +6,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import salute.oneshot.domain.cocktail.dto.response.CocktailResponseDto;
 import salute.oneshot.domain.cocktail.service.CocktailService;
 import salute.oneshot.domain.common.dto.success.ApiResponse;
@@ -31,5 +28,13 @@ public class CocktailController {
         Page<CocktailResponseDto> responsePage = cocktailService.getAllCocktails(pageable);
 
         return ResponseEntity.ok(ApiResponse.success(ApiResponseMessage.GET_CCKTL_LIST_SUCCESS, responsePage));
+    }
+
+    @GetMapping("/{cocktailId}")
+    public ResponseEntity<ApiResponse<CocktailResponseDto>> getCocktailById(@PathVariable(name = "cocktailId") Long cocktailId){
+
+        CocktailResponseDto responseDto = cocktailService.getCocktailById(cocktailId);
+
+        return ResponseEntity.ok(ApiResponse.success(ApiResponseMessage.GET_CCKTL_LIST_SUCCESS, responseDto));
     }
 }
--- src/main/java/salute/oneshot/domain/cocktail/service/CocktailService.java
+++ src/main/java/salute/oneshot/domain/cocktail/service/CocktailService.java
@@ -8,6 +8,8 @@ import salute.oneshot.domain.cocktail.dto.response.CocktailResponseDto;
 import salute.oneshot.domain.cocktail.entity.Cocktail;
 import salute.oneshot.domain.cocktail.repository.CocktailQueryDslRepository;
 import salute.oneshot.domain.cocktail.repository.CocktailRepository;
+import salute.oneshot.domain.common.dto.error.ErrorCode;
+import salute.oneshot.global.exception.NotFoundException;
 
 @Service
 @RequiredArgsConstructor
@@ -15,12 +17,24 @@ public class CocktailService {
 
     private final CocktailRepository cocktailRepository;
 
+
     public Page<CocktailResponseDto> getAllCocktails(Pageable pageable){
 
        Page<Cocktail> cocktailPage = cocktailRepository.findAll(pageable);
        Page<CocktailResponseDto> cocktailResponseDtoPage = cocktailPage.map(CocktailResponseDto::from);
 
        return cocktailResponseDtoPage;
+    }
+
+
+    public CocktailResponseDto getCocktailById(Long cocktailId){
+
+        Cocktail cocktail = cocktailRepository.findById(cocktailId)
+                .orElseThrow(()-> new NotFoundException(ErrorCode.COCKTAIL_NOT_FOUND));
+
+        CocktailResponseDto responseDto = CocktailResponseDto.from(cocktail);
+
+        return responseDto;
     }
 
 
