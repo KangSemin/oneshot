55b9d70f1fad1ea097fe6201a6c6881c156c6d9f
goo3oo <goo3oo0918@gmail.com>
2025-02-12 19:59:58 +0900

    feat: 유저 탈퇴 기능 추가 (softDelete)

--- src/main/java/salute/oneshot/domain/user/repository/UserRepository.java
+++ src/main/java/salute/oneshot/domain/user/repository/UserRepository.java
@@ -9,5 +9,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     boolean existsByEmail(String email);
 
-    Optional<User> findByEmail(String email);
+    Optional<User> findByEmailAndDeletedFalse(String email);
+
+    Optional<User> findByIdAndDeletedFalse(Long id);
 }
--- src/main/java/salute/oneshot/domain/user/service/UserService.java
+++ src/main/java/salute/oneshot/domain/user/service/UserService.java
@@ -19,20 +19,34 @@ public class UserService {
     private final PasswordEncoder passwordEncoder;
 
     @Transactional(readOnly = true)
-    public UserResponseDto getUserInfo(Long id) {
-        User user = userRepository.findById(id)
-                .orElseThrow(() -> new NotFoundException(ErrorCode.USER_NOT_FOUND));
+    public UserResponseDto getUserInfo(Long userId) {
+        User user = getUserById(userId);
+
         return UserResponseDto.from(user);
     }
 
     @Transactional
     public UserResponseDto updateUserInfo(UserUpdateSDto userUpdateSDto) {
-        User user = userRepository.findById(userUpdateSDto.getId())
-                .orElseThrow(() -> new NotFoundException(ErrorCode.USER_NOT_FOUND));
+        User user = getUserById(userUpdateSDto.getId());
         user.update(
                 userUpdateSDto.getNickName(),
                 passwordEncoder.encode(userUpdateSDto.getPassword())
         );
+
         return UserResponseDto.from(user);
+    }
+
+    @Transactional
+    public UserResponseDto deleteUser(Long userId) {
+        User user = getUserById(userId);
+        user.softDelete();
+
+        return UserResponseDto.from(user);
+    }
+
+    private User getUserById(Long userId) {
+        return userRepository.findByIdAndDeletedFalse(userId)
+                .orElseThrow(() ->
+                        new NotFoundException(ErrorCode.USER_NOT_FOUND));
     }
 }
