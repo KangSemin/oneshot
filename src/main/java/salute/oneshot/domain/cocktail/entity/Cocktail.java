d69f1ccceeb4bc52aecdba9e8a18d1addc3bbd4e
lee sujin <jin18302@naver.com>
2025-02-13 21:46:13 +0900

    fix: 실험했던 코드 삭제

--- src/main/java/salute/oneshot/domain/cocktail/entity/Cocktail.java
+++ src/main/java/salute/oneshot/domain/cocktail/entity/Cocktail.java
@@ -1,8 +1,17 @@
 package salute.oneshot.domain.cocktail.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import jakarta.persistence.*;
-
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import java.util.List;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -15,7 +24,6 @@ import salute.oneshot.domain.user.entity.User;
 @Table(name = "cocktails")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Cocktail extends BaseEntity {
 
 
@@ -35,7 +43,7 @@ public class Cocktail extends BaseEntity {
     @JoinColumn(name = "user_id")
     private User user;
 
-    @OneToMany(mappedBy = "cocktail", cascade = CascadeType.PERSIST)
+    @OneToMany(mappedBy = "cocktail")
     private List<CocktailIngredient> ingredientList;
 
     private Integer likeCounts;
--- src/main/java/salute/oneshot/domain/cocktail/entity/CocktailIngredient.java
+++ src/main/java/salute/oneshot/domain/cocktail/entity/CocktailIngredient.java
@@ -1,6 +1,15 @@
 package salute.oneshot.domain.cocktail.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -21,7 +30,7 @@ public class CocktailIngredient {
     @JoinColumn(name = "cocktail_id")
     private Cocktail cocktail;
 
-    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
+    @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "ingredient_id")
     private Ingredient ingredient;
 
