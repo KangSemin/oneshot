fe0b37ae0ff0115a791e1b745522c47f67c81cc7
lee sujin <jin18302@naver.com>
2025-02-13 22:17:16 +0900

    feat: ì 조건별 검색 추가

---
+++ src/main/java/salute/oneshot/domain/cocktail/dto/service/findCocktailSDto.java
@@ -0,0 +1,21 @@
+package salute.oneshot.domain.cocktail.dto.service;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.data.domain.Pageable;
+
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class findCocktailSDto {
+    private Pageable pageable;
+    private String keyword;
+    private String recipeType;
+
+
+    public static findCocktailSDto of(Pageable pageable, String keyword, String recipeType) {
+        return new findCocktailSDto(pageable, keyword, recipeType);
+    }
+
+}
