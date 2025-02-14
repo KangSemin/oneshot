77714f38fe4871b64b21e73a2648c8e911df8824
dornrrl0932 <dornrrl0932@naver.com>
2025-02-14 09:28:15 +0900

    fix: @AllArgsConstructor 추가

--- src/main/java/salute/oneshot/domain/ingredientReview/dto/service/CreateIngrReviewSDto.java
+++ src/main/java/salute/oneshot/domain/ingredientReview/dto/service/CreateIngrReviewSDto.java
@@ -1,21 +1,16 @@
 package salute.oneshot.domain.ingredientReview.dto.service;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class CreateIngrReviewSDto {
 
     private Long userId;
     private Long ingredientId;
     private Byte star;
     private String content;
-
-    private CreateIngrReviewSDto(Long userId, Long ingredientId, Byte star, String content) {
-        this.userId = userId;
-        this.ingredientId = ingredientId;
-        this.star = star;
-        this.content = content;
-    }
 
     public static CreateIngrReviewSDto of(Long userId, Long ingredientId, Byte star, String content) {
         return new CreateIngrReviewSDto(userId, ingredientId, star, content);
