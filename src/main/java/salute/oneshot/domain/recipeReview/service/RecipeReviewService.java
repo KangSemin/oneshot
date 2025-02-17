0260970ad37161bfaa9345dbeb9778d4f3a8f74a
dornrrl0932 <dornrrl0932@naver.com>
2025-02-17 12:50:27 +0900

    refactor: ResponseDto에 유저, 칵테일 정보 추가

--- src/main/java/salute/oneshot/domain/recipeReview/service/RecipeReviewService.java
+++ src/main/java/salute/oneshot/domain/recipeReview/service/RecipeReviewService.java
@@ -1,13 +1,15 @@
 package salute.oneshot.domain.recipeReview.service;
 
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import salute.oneshot.domain.cocktail.entity.Cocktail;
 import salute.oneshot.domain.cocktail.repository.CocktailRepository;
 import salute.oneshot.domain.common.dto.error.ErrorCode;
+import salute.oneshot.domain.recipeReview.dto.response.CocktailResponseDto;
 import salute.oneshot.domain.recipeReview.dto.response.RecipeReviewResponseDto;
+import salute.oneshot.domain.recipeReview.dto.response.UserResponseDto;
 import salute.oneshot.domain.recipeReview.dto.service.*;
 import salute.oneshot.domain.recipeReview.entity.RecipeReview;
 import salute.oneshot.domain.recipeReview.repository.RecipeReviewRepository;
@@ -32,28 +34,46 @@ public class RecipeReviewService {
         Cocktail cocktail = cocktailRepository.findById(sDto.getCocktailId())
                 .orElseThrow(() -> new NotFoundException(ErrorCode.COCKTAIL_NOT_FOUND));
 
-        RecipeReview recipeReview = recipeReviewRepository.save(RecipeReview.of(sDto.getStar(),sDto.getContent(), user, cocktail));
+        RecipeReview recipeReview = recipeReviewRepository.save(RecipeReview.of(sDto.getStar(), sDto.getContent(), user, cocktail));
 
-        return RecipeReviewResponseDto.from(recipeReview);
+        UserResponseDto userResponseDto = UserResponseDto.from(user);
+        CocktailResponseDto cocktailResponseDto = CocktailResponseDto.from(cocktail);
+
+        return RecipeReviewResponseDto.from(cocktailResponseDto, userResponseDto, recipeReview);
     }
 
+    @Transactional
     public Page<RecipeReviewResponseDto> getMyRecipeReview(GetMyRecipeReviewSDto sDto) {
 
         Page<RecipeReview> recipeReviewPage = recipeReviewRepository.findAllByUser_Id(sDto.getUserId(), sDto.getPageable());
 
-        Page<RecipeReviewResponseDto> responseDtoPage = recipeReviewPage.map(RecipeReviewResponseDto::from);
+        Page<RecipeReviewResponseDto> responseDtoPage = recipeReviewPage.map(recipeReview -> {
+
+            UserResponseDto userResponseDto = UserResponseDto.from(recipeReview.getUser());
+            CocktailResponseDto cocktailResponseDto = CocktailResponseDto.from(recipeReview.getCocktail());
+
+            return RecipeReviewResponseDto.from(cocktailResponseDto, userResponseDto, recipeReview);
+        });
 
         return responseDtoPage;
     }
 
+    @Transactional
     public RecipeReviewResponseDto getRecipeReview(Long reviewId) {
 
         RecipeReview recipeReview = recipeReviewRepository.findById(reviewId)
                 .orElseThrow(() -> new NotFoundException(ErrorCode.REVIEW_NOT_FOUND));
 
-        return RecipeReviewResponseDto.from(recipeReview);
+        User user = recipeReview.getUser();
+        Cocktail cocktail = recipeReview.getCocktail();
+
+        UserResponseDto userResponseDto = UserResponseDto.from(user);
+        CocktailResponseDto cocktailResponseDto = CocktailResponseDto.from(cocktail);
+
+        return RecipeReviewResponseDto.from(cocktailResponseDto, userResponseDto, recipeReview);
     }
 
+    @Transactional
     public Page<RecipeReviewResponseDto> getAllRecipeReview(GetAllRecipeReviewSDto sDto) {
 
         cocktailRepository.findById(sDto.getCocktailId())
@@ -61,7 +81,13 @@ public class RecipeReviewService {
 
         Page<RecipeReview> recipeReviewPage = recipeReviewRepository.findAllByCocktail_Id(sDto.getCocktailId(), sDto.getPageable());
 
-        Page<RecipeReviewResponseDto> responseDtoPage = recipeReviewPage.map(RecipeReviewResponseDto::from);
+        Page<RecipeReviewResponseDto> responseDtoPage = recipeReviewPage.map(recipeReview -> {
+
+            UserResponseDto userResponseDto = UserResponseDto.from(recipeReview.getUser());
+            CocktailResponseDto cocktailResponseDto = CocktailResponseDto.from(recipeReview.getCocktail());
+
+            return RecipeReviewResponseDto.from(cocktailResponseDto, userResponseDto, recipeReview);
+        });
 
         return responseDtoPage;
     }
@@ -72,13 +98,19 @@ public class RecipeReviewService {
         RecipeReview recipeReview = recipeReviewRepository.findById(sDto.getReviewId())
                 .orElseThrow(() -> new NotFoundException(ErrorCode.REVIEW_NOT_FOUND));
 
-        if(!recipeReview.getUser().getId().equals(sDto.getUserId())) {
+        if (!recipeReview.getUser().getId().equals(sDto.getUserId())) {
             throw new CustomRuntimeException(ErrorCode.REVIEW_UPDATE_FORBIDDEN);
         }
 
-        recipeReview.updateRecipeReview(sDto.getStar(),sDto.getContent());
+        recipeReview.updateRecipeReview(sDto.getStar(), sDto.getContent());
 
-        return RecipeReviewResponseDto.from(recipeReview);
+        User user = recipeReview.getUser();
+        Cocktail cocktail = recipeReview.getCocktail();
+
+        UserResponseDto userResponseDto = UserResponseDto.from(user);
+        CocktailResponseDto cocktailResponseDto = CocktailResponseDto.from(cocktail);
+
+        return RecipeReviewResponseDto.from(cocktailResponseDto, userResponseDto, recipeReview);
     }
 
     public void deleteRecipeReview(DeleteRecipeReviewSDto sDto) {
@@ -86,7 +118,7 @@ public class RecipeReviewService {
         RecipeReview recipeReview = recipeReviewRepository.findById(sDto.getReviewId())
                 .orElseThrow(() -> new NotFoundException(ErrorCode.REVIEW_NOT_FOUND));
 
-        if(!recipeReview.getUser().getId().equals(sDto.getUserId())) {
+        if (!recipeReview.getUser().getId().equals(sDto.getUserId())) {
             throw new CustomRuntimeException(ErrorCode.REVIEW_DELETE_FORBIDDEN);
         }
 
