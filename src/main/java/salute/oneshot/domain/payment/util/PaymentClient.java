1242aba557fcac7e1f7f3c02dded573507546a3b
cnux9 <cnux9@naver.com>
2025-02-18 11:47:45 +0900

    comment: remove useless comments

--- src/main/java/salute/oneshot/domain/payment/util/PaymentClient.java
+++ src/main/java/salute/oneshot/domain/payment/util/PaymentClient.java
@@ -7,12 +7,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import salute.oneshot.domain.payment.dto.request.ConfirmPaymentRequestDto;
 import salute.oneshot.domain.payment.dto.response.ConfirmPaymentResponseDto;
 import salute.oneshot.domain.payment.dto.service.ConfirmPaymentSDto;
-import salute.oneshot.global.config.PaymentFeignConfig;
+import salute.oneshot.global.config.FeignConfig;
 
 //@FeignClient(name = "paymentClient", url = "${payment.base-url}", configuration = PaymentFeignConfig.class)
-@FeignClient(name = "paymentClient", url = "https://api.tosspayments.com/v1/payments", configuration = PaymentFeignConfig.class)
+@FeignClient(name = "paymentClient", url = "https://api.tosspayments.com/v1/payments", configuration = FeignConfig.class)
 public interface PaymentClient {
-
     @PostMapping(value = "/confirm", consumes = MediaType.APPLICATION_JSON_VALUE)
     ConfirmPaymentResponseDto confirmPayment(@RequestBody ConfirmPaymentSDto sdto);
 
--- src/main/java/salute/oneshot/global/config/FeignConfig.java
+++ src/main/java/salute/oneshot/global/config/FeignConfig.java
@@ -1,19 +1,17 @@
 package salute.oneshot.global.config;
 
 import feign.Request;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import salute.oneshot.domain.payment.util.PaymentAuthInterceptor;
 import salute.oneshot.domain.payment.util.PaymentClient;
-import salute.oneshot.domain.payment.util.PaymentErrorDecoder;
 
 import java.util.concurrent.TimeUnit;
 
 @Configuration
 @EnableFeignClients(clients = PaymentClient.class)
-public class PaymentFeignConfig {
+public class FeignConfig {
 
     @Bean
     public Request.Options requestOptions() {
