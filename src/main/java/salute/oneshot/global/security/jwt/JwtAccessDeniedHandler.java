059429003dff30d6fb1ade21d4840aa1e5d15fa1
goo3oo <goo3oo0918@gmail.com>
2025-02-21 16:48:52 +0900

    fix: 불필요한 임포트 삭제

--- src/main/java/salute/oneshot/global/security/jwt/JwtAccessDeniedHandler.java
+++ src/main/java/salute/oneshot/global/security/jwt/JwtAccessDeniedHandler.java
@@ -9,7 +9,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 import salute.oneshot.domain.common.dto.error.ErrorCode;
 import salute.oneshot.domain.common.dto.error.ErrorResponse;
 import salute.oneshot.global.security.SecurityConst;
