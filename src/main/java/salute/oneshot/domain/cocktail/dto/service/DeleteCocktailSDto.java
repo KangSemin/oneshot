4ac7d40a702199cd1568e8d2d692db3dd1a44b18
강세민 <semin15151@gmail.com>
2025-02-24 20:44:36 +0900

    fix : 칵테일 생성 및 삭제 api에 권한 관련  로직 추가

--- src/main/java/salute/oneshot/domain/cocktail/dto/service/DeleteCocktailSDto.java
+++ src/main/java/salute/oneshot/domain/cocktail/dto/service/DeleteCocktailSDto.java
@@ -4,16 +4,18 @@ package salute.oneshot.domain.cocktail.dto.service;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import salute.oneshot.domain.user.entity.UserRole;
 
 @Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class DeleteCocktailSDto {
     private final Long userId;
+    private final UserRole userRole;
     private final Long cocktailId;
 
 
-    public static DeleteCocktailSDto of(Long userId, Long cocktailId) {
-        return new DeleteCocktailSDto(userId,cocktailId);
+    public static DeleteCocktailSDto of(Long userId, UserRole userRole, Long cocktailId) {
+        return new DeleteCocktailSDto(userId, userRole,cocktailId);
     }
 
 }
