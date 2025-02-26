a7c2c52cad0273fb2e1611b342fdd62447ba90b4
cnux9 <cnux9@naver.com>
2025-02-26 22:32:11 +0900

    fix: 관리자 채팅창에서 다른 채팅창의 메시지도 오던 버그 픽스

--- src/main/java/salute/oneshot/domain/chat/controller/ChatController.java
+++ src/main/java/salute/oneshot/domain/chat/controller/ChatController.java
@@ -2,9 +2,8 @@ package salute.oneshot.domain.chat.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import salute.oneshot.domain.chat.dto.response.FindChatResponseDto;
 import salute.oneshot.domain.chat.dto.response.FindChatsResponseDto;
 import salute.oneshot.domain.chat.service.ChatService;
 import salute.oneshot.global.security.entity.CustomUserDetails;
@@ -14,20 +13,20 @@ import salute.oneshot.global.security.entity.CustomUserDetails;
 public class ChatController {
     private final ChatService chatService;
 
-//    @GetMapping("/api/chats")
-//    public FindChatResponseDto findChat(
-//            @AuthenticationPrincipal CustomUserDetails userDetails
-//    ) {
-//        return chatService.findChat(userDetails.getId());
-//    }
-//
-//    @PostMapping("/api/chats")
-//    public void processMessageFromClient(
-//            @RequestBody String message,
-//            @AuthenticationPrincipal CustomUserDetails userDetails
-//    ) {
-//        chatService.processMessageFromClient(message, userDetails.getId(), userDetails.getUserRole());
-//    }
+    @GetMapping("/api/chats")
+    public FindChatResponseDto findChat(
+            @AuthenticationPrincipal CustomUserDetails userDetails
+    ) {
+        return chatService.findChat(userDetails.getId());
+    }
+
+    @GetMapping("/api/admin/chats/{userId}")
+    public FindChatResponseDto findChat(
+            @AuthenticationPrincipal CustomUserDetails userDetails,
+            @PathVariable Long userId
+    ) {
+        return chatService.findChatForAdmin(userDetails.getId(), userId);
+    }
 
     @GetMapping("/api/admin/chats")
     public FindChatsResponseDto findChats(
