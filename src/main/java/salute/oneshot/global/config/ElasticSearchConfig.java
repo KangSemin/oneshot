834359be827ce76f1d78a3195194869c911b86bb
강세민 <semin15151@gmail.com>
2025-02-26 16:56:45 +0900

    refactor : 코트 리팩터링 및 RecipeType에 따른 필터링 기능 추가

--- src/main/java/salute/oneshot/global/config/ElasticSearchConfig.java
+++ src/main/java/salute/oneshot/global/config/ElasticSearchConfig.java
@@ -1,5 +1,6 @@
 package salute.oneshot.global.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
@@ -9,6 +10,8 @@ import org.springframework.data.elasticsearch.repository.config.EnableElasticsea
 @EnableElasticsearchRepositories
 public class ElasticSearchConfig extends ElasticsearchConfiguration {
 
+    @Value("${ELASTICSEARCH_HOST}")
+    private String[] hosts;
 
     @Override
     public ClientConfiguration clientConfiguration() {
