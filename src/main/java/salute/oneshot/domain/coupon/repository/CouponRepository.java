31aaa9e52c164b6fb7ff084fb883684f9712ecfc
goo3oo <goo3oo0918@gmail.com>
2025-02-26 09:33:55 +0900

    feat: 쿠폰 삭제 기능 추가

--- src/main/java/salute/oneshot/domain/coupon/repository/CouponRepository.java
+++ src/main/java/salute/oneshot/domain/coupon/repository/CouponRepository.java
@@ -1,8 +1,14 @@
 package salute.oneshot.domain.coupon.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import salute.oneshot.domain.coupon.entity.Coupon;
 
 public interface CouponRepository extends JpaRepository<Coupon, Long> {
 
+    @Modifying
+    @Query("DELETE FROM Coupon c WHERE c.id = :couponId")
+    int deleteCouponById(@Param("couponId") Long couponId);
 }
