822fe1aa64fe67bf0dff07ab9af336bae884321a
goo3oo <goo3oo0918@gmail.com>
2025-02-27 11:07:50 +0900

    feat: event 조회 기능 추가

--- src/main/java/salute/oneshot/domain/event/service/EventService.java
+++ src/main/java/salute/oneshot/domain/event/service/EventService.java
@@ -1,17 +1,25 @@
 package salute.oneshot.domain.event.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import salute.oneshot.domain.common.dto.error.ErrorCode;
 import salute.oneshot.domain.event.dto.response.EventBriefResponseDto;
+import salute.oneshot.domain.event.dto.response.EventDetailResponseDto;
+import salute.oneshot.domain.event.dto.response.EventPageResponseDto;
 import salute.oneshot.domain.event.dto.service.CreateEventSDto;
+import salute.oneshot.domain.event.dto.service.GetEventsSDto;
 import salute.oneshot.domain.event.entity.Event;
 import salute.oneshot.domain.event.entity.EventDetail;
 import salute.oneshot.domain.event.procssor.EventProcessor;
 import salute.oneshot.domain.event.procssor.EventProcessorFactory;
 import salute.oneshot.domain.event.repository.EventRepository;
+import salute.oneshot.global.exception.NotFoundException;
 
 @Slf4j
 @Service
@@ -40,5 +48,42 @@ public class EventService {
 
         eventRepository.save(event);
         return EventBriefResponseDto.from(event);
+    }
+
+    @Transactional(readOnly = true)
+    public EventDetailResponseDto getEvent(Long eventId) {
+        Event event = eventRepository.findById(eventId)
+                .orElseThrow(() ->
+                        new NotFoundException(ErrorCode.EVENT_NOT_FOUND));
+
+        Object eventDetail =
+                parseJsonToNode(event.getEventDetail().getDetailData());
+
+        return EventDetailResponseDto.of(event, eventDetail);
+    }
+
+    @Transactional(readOnly = true)
+    public EventPageResponseDto getEvents(GetEventsSDto serviceDto) {
+        Page<Event> events = eventRepository.findEvents(
+                serviceDto.getStatus(),
+                serviceDto.getType(),
+                serviceDto.getStartTime(),
+                serviceDto.getEndTime(),
+                serviceDto.getPageable());
+
+        Page<EventBriefResponseDto> eventPage =
+                events.map(EventBriefResponseDto::from);
+
+        return EventPageResponseDto.from(eventPage);
+    }
+
+    // DB에 저장된 JSON 계층 구조로 변환
+    private JsonNode parseJsonToNode(String json) {
+        try {
+            return objectMapper.readTree(json);
+        } catch (JsonProcessingException e) {
+            log.error("JSON 파싱 오류: {}", e.getMessage(), e);
+            throw new RuntimeException("JSON 파싱 오류", e);
+        }
     }
 }
