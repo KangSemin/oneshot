d75394871328139242fb3a0906a77f06ee9cc25a
goo3oo <goo3oo0918@gmail.com>
2025-02-27 12:44:57 +0900

    feat: event 삭제 기능 추가

--- src/main/java/salute/oneshot/domain/event/repository/EventRepository.java
+++ src/main/java/salute/oneshot/domain/event/repository/EventRepository.java
@@ -3,6 +3,7 @@ package salute.oneshot.domain.event.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import salute.oneshot.domain.event.entity.Event;
@@ -24,4 +25,8 @@ public interface EventRepository extends JpaRepository<Event, Long> {
             @Param("startTime") LocalDateTime startTime,
             @Param("endTime") LocalDateTime endTime,
             Pageable pageable);
+
+    @Modifying
+    @Query("DELETE FROM Event e WHERE e.id = :eventId")
+    int deleteEventById(@Param("eventId") Long eventId);
 }
