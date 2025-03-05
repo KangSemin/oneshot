cebeea2566f7465fcff185b3dae2a7ea2497586d
cnux9 <cnux9@naver.com>
2025-03-05 17:36:28 +0900

    feat: 이미지 업로드 기능 추가

--- src/main/java/salute/oneshot/domain/cocktail/controller/CocktailController.java
+++ src/main/java/salute/oneshot/domain/cocktail/controller/CocktailController.java
@@ -1,8 +1,5 @@
 package salute.oneshot.domain.cocktail.controller;
 
-import java.io.IOException;
-import java.util.List;
-
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
@@ -11,7 +8,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-import salute.oneshot.domain.cocktail.dto.request.CreateCocktailRequestDto;
+import org.springframework.web.multipart.MultipartFile;
+import salute.oneshot.domain.cocktail.dto.request.CreateCocktailForUploadRequestDto;
 import salute.oneshot.domain.cocktail.dto.request.SearchCocktailByIngrsReqDto;
 import salute.oneshot.domain.cocktail.dto.request.UpdateCocktailRequestDto;
 import salute.oneshot.domain.cocktail.dto.response.CocktailResponseDto;
@@ -21,6 +19,10 @@ import salute.oneshot.domain.cocktail.service.CocktailService;
 import salute.oneshot.domain.common.dto.success.ApiResponse;
 import salute.oneshot.domain.common.dto.success.ApiResponseConst;
 import salute.oneshot.global.security.entity.CustomUserDetails;
+import salute.oneshot.global.util.S3Uploader;
+
+import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/cocktails")
@@ -28,11 +30,46 @@ import salute.oneshot.global.security.entity.CustomUserDetails;
 public class CocktailController {
 
     private final CocktailService cocktailService;
+    private final S3Uploader s3Uploader;
 
+//    @PostMapping
+//    public ResponseEntity<ApiResponse<CocktailResponseDto>> createCocktail(
+//        @RequestBody CreateCocktailRequestDto request,
+//        @AuthenticationPrincipal CustomUserDetails userDetails) {
+//
+//        CreateCocktailSDto sDto = CreateCocktailSDto.of(userDetails.getId(),
+//            userDetails.getUserRole(), request.getName(),
+//            request.getDescription(), request.getRecipe(), request.getIngredientList());
+//
+//        cocktailService.createCocktail(sDto);
+//
+//        return ResponseEntity.ok(ApiResponse.success(ApiResponseConst.ADD_RCP_SUCCESS));
+//    }
+
+    // 이미지 업로드용 API
+    // MultipartFile을 사용하는 방식은 Form 형태로 받기 위해서 @ModelAttribute 사용
+    // TODO:
     @PostMapping
-    public ResponseEntity<ApiResponse<CocktailResponseDto>> createCocktail(
-        @RequestBody CreateCocktailRequestDto request,
+    public ResponseEntity<ApiResponse<CocktailResponseDto>> createCocktailForUpload(
+        @ModelAttribute CreateCocktailForUploadRequestDto request,
         @AuthenticationPrincipal CustomUserDetails userDetails) {
+
+        // 예시코드
+        // S3Uploader 빈 의존성 주입 필요
+        MultipartFile imageFile = request.getImageFile();
+
+        String imageFileName;
+        if (imageFile != null) {
+            try {
+                imageFileName = s3Uploader.upload(imageFile);
+            } catch (IOException e) {
+                // IOException 예외처리
+            }
+        }
+        // 파일 이름을 엔티티에 함께 저장
+        // Url ex) https://oneshot-bucket2.s3.ap-northeast-2.amazonaws.com/(파일이름).jpg
+        // 사진을 조회할 때 -> amazonS3.getUrl(bucketName, s3FileName).toString()
+
 
         CreateCocktailSDto sDto = CreateCocktailSDto.of(userDetails.getId(),
             userDetails.getUserRole(), request.getName(),
