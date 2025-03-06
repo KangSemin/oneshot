860d2c994009255fe211e1a68b1e319c65a92d49
lee sujin <jin18302@naver.com>
2025-03-06 12:43:04 +0900

    feature: 재료 생성 프론트엔드 구현

--- src/main/java/salute/oneshot/domain/ingredient/controller/IngredientViewController.java
+++ src/main/java/salute/oneshot/domain/ingredient/controller/IngredientViewController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import salute.oneshot.global.config.NonceGenerator;
 
 @Controller
@@ -21,4 +22,6 @@ public class IngredientViewController {
         model.addAttribute("scriptNonce", nonceGenerator.getNonce());
         return "ingredient/create-ingredient";
     }
+
+    
 }
