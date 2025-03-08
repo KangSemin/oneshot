675efdc4927d4416e3c5a06da4b3df56afbaf53b
goo3oo <goo3oo0918@gmail.com>
2025-03-09 00:34:17 +0900

    test: 테스트용 시큐리티 설정 - 기존 시큐리티 설정을 읽어오지 못해서 따로 설정필요 - csrf = disable - anyRequest().permitAll()

---
+++ src/test/java/salute/oneshot/config/TestSecurityConfig.java
@@ -0,0 +1,27 @@
+package salute.oneshot.config;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.context.DelegatingSecurityContextRepository;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
+import org.springframework.security.web.context.RequestAttributeSecurityContextRepository;
+
+@TestConfiguration
+public class TestSecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll())
+                .securityContext(securityContext -> securityContext
+                        .securityContextRepository(
+                                new DelegatingSecurityContextRepository(
+                                new HttpSessionSecurityContextRepository(),
+                                new RequestAttributeSecurityContextRepository())))
+                .build();
+    }
+}
