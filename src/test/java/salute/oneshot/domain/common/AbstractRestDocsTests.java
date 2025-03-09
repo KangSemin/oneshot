9341be1a52929a70fae706c0b831e18e0b234508
강세민 <semin15151@gmail.com>
2025-03-09 15:41:01 +0900

    script : adoc 문서 수정

--- src/test/java/salute/oneshot/domain/common/AbstractRestDocsTests.java
+++ src/test/java/salute/oneshot/domain/common/AbstractRestDocsTests.java
@@ -7,15 +7,19 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
 import salute.oneshot.config.RestDocsConfiguration;
+import salute.oneshot.global.security.filter.JwtFilter;
+import salute.oneshot.global.security.jwt.JwtProvider;
 
 @Import(RestDocsConfiguration.class)
 @ExtendWith(RestDocumentationExtension.class)
@@ -26,6 +30,15 @@ public abstract class AbstractRestDocsTests {
 
     @Autowired
     protected MockMvc mockMvc;
+
+    @MockitoBean
+    private JpaMetamodelMappingContext jpaMetamodelMappingContext;
+
+    @MockitoBean
+    private JwtProvider jwtProvider;
+
+    @MockitoBean
+    private JwtFilter jwtFilter;
 
     @BeforeEach
     void setUp(
