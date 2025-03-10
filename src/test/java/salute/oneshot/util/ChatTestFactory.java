0656fcee3730ec22e7832f923433a9621ff23631
cnux9 <cnux9@naver.com>
2025-03-10 12:35:45 +0900

    test: Factory 리터럴 전역 상수 처리

--- src/test/java/salute/oneshot/util/ChatTestFactory.java
+++ src/test/java/salute/oneshot/util/ChatTestFactory.java
@@ -11,9 +11,10 @@ import java.util.List;
 public class ChatTestFactory {
 
     public static final Long USER_ID = 1L;
+    public static final String RAW_MESSAGE = "u::메시지 입니다.::1741314450785";
 
     public static FindChatResponseDto createFindChatResponseDto() {
-        MessageResponseDto message = MessageResponseDto.from("u::메시지 입니다.::1741314450785");
+        MessageResponseDto message = MessageResponseDto.from(RAW_MESSAGE);
 
         List<MessageResponseDto> messageList = new ArrayList<>();
         messageList.add(message);
@@ -23,14 +24,13 @@ public class ChatTestFactory {
 
     public static FindChatListResponseDto createFindChatListResponseDto() {
         long userId = 2L;
-        String lastMessage = "u::가장 최근 메시지 입니다.::1741314450785";
 
-        ChatPreviewResponseDto chatPreviewResponseDto = ChatPreviewResponseDto.of(Long.toString(userId), lastMessage);
+        ChatPreviewResponseDto chatPreviewResponseDto = ChatPreviewResponseDto.of(Long.toString(userId), RAW_MESSAGE);
 
         List<ChatPreviewResponseDto> chatList = new ArrayList<>();
         chatList.add(chatPreviewResponseDto);
 
-        String nextCursor = "몰루";
+        String nextCursor = "";
         return FindChatListResponseDto.of(chatList, nextCursor);
     }
 }
