78a2a3486792430c637c4f62efd2c41aa8ce6754
goo3oo <goo3oo0918@gmail.com>
2025-03-11 14:39:04 +0900

    refactor: 정적팩토리 메서드 추가(테스트용)

--- src/main/java/salute/oneshot/domain/banner/dto/request/BannerRequestDto.java
+++ src/main/java/salute/oneshot/domain/banner/dto/request/BannerRequestDto.java
@@ -14,4 +14,21 @@ public class BannerRequestDto {
     private final String startTime;
     private final String endDate;
     private final String endTime;
+
+    public static BannerRequestDto of(
+            Long bannerId,
+            String imageUrl,
+            String startDate,
+            String startTime,
+            String endDate,
+            String endTime
+    ) {
+        return new BannerRequestDto(
+                bannerId,
+                imageUrl,
+                startDate,
+                startTime,
+                endDate,
+                endTime);
+    }
 }
