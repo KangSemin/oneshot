fd4c16e491280f8bd218be11b1dbb2b531ffdd79
cnux9 <cnux9@naver.com>
2025-03-11 16:09:39 +0900

    comment: 필요없는 주석 제거

--- src/test/java/salute/oneshot/util/IngredientReviewTestFactory.java
+++ src/test/java/salute/oneshot/util/IngredientReviewTestFactory.java
@@ -51,60 +51,6 @@ public class IngredientReviewTestFactory {
 
         return constructor.newInstance(STAR, CONTENT);
     }
-//    public static final Byte STAR = 5;
-//    public static final String CONTENT = "맛있어요!";
-//
-//    public static RecipeReviewResponseDto createRecipeReviewResponseDto() throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
-//        Constructor<RecipeReviewResponseDto> constructor =
-//                RecipeReviewResponseDto.class.getDeclaredConstructor(CocktailResponseDto.class, UserResponseDto.class, Long.class, Byte.class, String.class);
-//        constructor.setAccessible(true);
-//
-//        return constructor.newInstance(createCocktailResponseDto(), createUserResponseDto(), RECIPE_REVIEW_ID, STAR, CONTENT);
-//    }
-//
-//    public static Page<RecipeReviewResponseDto> createRecipeReviewResponseDtoPage() throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
-//        List<RecipeReviewResponseDto> recipeReviewResponseDtoList = new ArrayList<>();
-//        recipeReviewResponseDtoList.add(createRecipeReviewResponseDto());
-//        return new PageImpl<>(recipeReviewResponseDtoList);
-//    }
-//
-//    public static CreateRecipeReviewRequestDto createCreateRecipeReviewRequestDto() throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
-//        Constructor<CreateRecipeReviewRequestDto> constructor =
-//                CreateRecipeReviewRequestDto.class.getDeclaredConstructor(Byte.class, String.class);
-//        constructor.setAccessible(true);
-//
-//        return constructor.newInstance(STAR, CONTENT);
-//    }
-//
-//    public static UpdateRecipeRequestDto createUpdateRecipeRequestDto() throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
-//        Constructor<UpdateRecipeRequestDto> constructor =
-//                UpdateRecipeRequestDto.class.getDeclaredConstructor(Byte.class, String.class);
-//        constructor.setAccessible(true);
-//
-//        return constructor.newInstance(STAR, CONTENT);
-//    }
-//
-//    // TODO: 도메인 분리 필요
-//    public static UserResponseDto createUserResponseDto() throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
-//        Constructor<UserResponseDto> constructor =
-//                UserResponseDto.class.getDeclaredConstructor(Long.class, String.class, String.class, UserRole.class);
-//        constructor.setAccessible(true);
-//
-//        return constructor.newInstance(UserTestFactory.USER_ID, UserTestFactory.EMAIL, UserTestFactory.NICKNAME, UserTestFactory.ROLE_USER);
-//    }
-//
-//    // TODO: 도메인 분리 필요
-//    public static CocktailResponseDto createCocktailResponseDto() throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
-//        List<IngredientResponseDto> ingredientList = new ArrayList<>();
-//        ingredientList.add(IngredientTestFactory.createIngredientResponseDto());
-//
-//        Constructor<CocktailResponseDto> constructor =
-//                CocktailResponseDto.class.getDeclaredConstructor(Long.class, String.class, String.class, String.class, RecipeType.class, Integer.class, Long.class);
-//        constructor.setAccessible(true);
-//
-//        return constructor.newInstance(CocktailTestFactory.COCKTAIL_ID, CocktailTestFactory.NAME, CocktailTestFactory.DESCRIPTION, CocktailTestFactory.RECIPE, CocktailTestFactory.TYPE, CocktailTestFactory.LIKE_COUNT, UserTestFactory.USER_ID);
-
-//    }
 
     // TODO: 도메인 분리 필요
     public static UserResponseDto createUserResponseDto() throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
