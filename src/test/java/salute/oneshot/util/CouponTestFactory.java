4fcba11d8d5143db3675a95a783b7a04cec313ef
goo3oo <goo3oo0918@gmail.com>
2025-03-11 11:35:10 +0900

    test: 쿠폰 컨트롤러 테스트 수정 - 쿠폰의 시작일, 종료일, 유저쿠폰의 상태기준으로 조회하는 상황 추가

--- src/test/java/salute/oneshot/util/CouponTestFactory.java
+++ src/test/java/salute/oneshot/util/CouponTestFactory.java
@@ -6,7 +6,6 @@ import salute.oneshot.domain.coupon.dto.request.UserCpnRequestDto;
 import salute.oneshot.domain.coupon.entity.Coupon;
 
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 public class CouponTestFactory {
 
@@ -16,11 +15,10 @@ public class CouponTestFactory {
     public static final int DISCOUNT_VALUE = 1000;
     public static final String START_DATE = "2025-03-01";
     public static final String START_TIME = "00:00";
-    public static final String END_DATE = "2025-04-01";
+    public static final String END_DATE = "2025-03-10";
     public static final String END_TIME = "00:00";
-    public static final LocalDateTime START_LOCAL_DATE_TIME = LocalDateTime.of(2025, 3, 1, 0, 0);
-    public static final LocalDateTime END_LOCAL_DATE_TIME = LocalDateTime.of(2025, 4, 1, 0, 0);
-    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm");
+    public static final LocalDateTime START_LOCAL_DATE_TIME = LocalDateTime.parse("2025-03-01T00:00:00");
+    public static final LocalDateTime END_LOCAL_DATE_TIME = LocalDateTime.parse("2025-03-10T00:00:00");
 
     public static Coupon createCoupon() {
         Coupon coupon = Coupon.of(
@@ -33,15 +31,33 @@ public class CouponTestFactory {
         return coupon;
     }
 
+    public static Coupon createCoupon2() {
+        Coupon coupon = Coupon.of(
+                "2000원 할인쿠폰",
+                2000,
+                LocalDateTime.parse("2025-03-05T00:00:00"),
+                LocalDateTime.parse("2025-03-11T00:00:00"));
+                ReflectionTestUtils.setField(coupon,"id",2L);
+
+        return coupon;
+    }
+
+    public static Coupon createCoupon3() {
+        Coupon coupon = Coupon.of(
+                "3000원 할인쿠폰",
+                3000,
+                LocalDateTime.parse("2025-03-02T00:00:00"),
+                LocalDateTime.parse("2025-03-09T00:00:00"));
+        ReflectionTestUtils.setField(coupon,"id",3L);
+
+        return coupon;
+    }
+
     public static CpnRequestDto createCpnRequestDto() {
         return CpnRequestDto.of(COUPON_NAME, DISCOUNT_VALUE, START_DATE, START_TIME, END_DATE, END_TIME );
     }
 
     public static UserCpnRequestDto createUserCpnRequestDTo() {
         return UserCpnRequestDto.of(COUPON_ID);
-    }
-
-    public static String formatDateTime(LocalDateTime dateTime) {
-        return dateTime.format(DATE_TIME_FORMATTER);
     }
 }
