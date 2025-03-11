f5a34ec854005f47d599c3eef0073c245acf417b
goo3oo <goo3oo0918@gmail.com>
2025-03-11 21:02:05 +0900

    move: BaseEntity 위치 변경 common/dto/BaseEntity -> common/entity/BaseEntity

--- src/main/java/salute/oneshot/domain/banner/entity/Banner.java
+++ src/main/java/salute/oneshot/domain/banner/entity/Banner.java
@@ -4,7 +4,7 @@ import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import salute.oneshot.domain.common.dto.entity.BaseEntity;
+import salute.oneshot.domain.common.entity.BaseEntity;
 import salute.oneshot.domain.event.entity.Event;
 
 import java.time.LocalDateTime;
