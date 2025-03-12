26a3b40942b7a36ccac6f8199b950b231f0110cc
강세민 <semin15151@gmail.com>
2025-03-12 17:32:49 +0900

    del : 불필요한 import 삭제

--- src/test/java/salute/oneshot/domain/common/AbstractRestDocsTests.java
+++ src/test/java/salute/oneshot/domain/common/AbstractRestDocsTests.java
@@ -5,8 +5,6 @@ import static org.springframework.security.test.web.servlet.setup.SecurityMockMv
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
@@ -24,14 +22,10 @@ import salute.oneshot.config.TestSecurityConfig;
 import salute.oneshot.global.security.filter.JwtFilter;
 import salute.oneshot.global.security.jwt.JwtProvider;
 
-
-
 @Import({RestDocsConfiguration.class,TestSecurityConfig.class})
 @ExtendWith(RestDocumentationExtension.class)
 public abstract class AbstractRestDocsTests {
 
-
-    private static final Logger log = LoggerFactory.getLogger(AbstractRestDocsTests.class);
     @Autowired
     protected RestDocumentationResultHandler restDocs;
 
