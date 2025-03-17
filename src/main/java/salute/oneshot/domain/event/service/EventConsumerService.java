866c20433c64d49f1aeb8e848bea6f156ddc30b5
goo3oo <goo3oo0918@gmail.com>
2025-03-17 17:27:24 +0900

    refactor: 로깅추가

--- src/main/java/salute/oneshot/domain/event/service/EventConsumerService.java
+++ src/main/java/salute/oneshot/domain/event/service/EventConsumerService.java
@@ -22,7 +22,7 @@ public class EventConsumerService {
 
         try {
             Long couponId = participateDto.getCouponId();
-            log.debug("쿠폰 발급 시도 - 쿠폰ID: {}, 사용자ID: {}", couponId, participateDto.getUserId());
+            log.info("쿠폰 발급 시도 - 쿠폰ID: {}, 사용자ID: {}", couponId, participateDto.getUserId());
 
             CreateUserCpnSDto createUserCpnSDto =
                     CreateUserCpnSDto.of(couponId, participateDto.getUserId());
