57779511dd4ce8a21c4e5ba1a031c91cd58dd287
lee sujin <jin18302@naver.com>
2025-06-29 16:07:49 +0900

    fix : 레디스 상수 수정

--- src/main/java/salute/oneshot/global/util/RedisConst.java
+++ src/main/java/salute/oneshot/global/util/RedisConst.java
@@ -5,11 +5,11 @@ import org.springframework.stereotype.Component;
 @Component
 public class RedisConst {
 
-    public static final String COCKTAIL_SCORE_KEY = "cocktail_score";
+    public static final String COCKTAIL_SCORE_KEY = "cocktail-score";
 
-    public static final String POPULAR_COCKTAIL_KEY = "popular_cocktail";
+    public static final String POPULAR_COCKTAIL_KEY = "popular-cocktail";
 
-    public static final String COCKTAIL_COUNT_KEY_PREFIX = "cocktail_count::";
+    public static final String COCKTAIL_VIEW_COUNT_KEY_PREFIX = "cocktail-view::";
 
     public static final String COCKTAIL_SCORE_KEY_PREFIX = "cocktail_score::";
 }
