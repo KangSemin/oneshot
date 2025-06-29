e455ee0b8af27cd509f871925093687ef3011b36
lee sujin <jin18302@naver.com>
2025-06-29 22:07:59 +0900

    fix : 예외처리 수정

--- src/main/java/salute/oneshot/global/util/HashMacUtil.java
+++ src/main/java/salute/oneshot/global/util/HashMacUtil.java
@@ -2,12 +2,15 @@ package salute.oneshot.global.util;
 
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
 import java.util.Base64;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class HashMacUtil {
@@ -26,13 +29,16 @@ public class HashMacUtil {
             byte[] hash = hmac.doFinal(message.getBytes());
             return Base64.getUrlEncoder().withoutPadding().encodeToString(hash);
         } catch (Exception e) {
-            throw new RuntimeException("HMAC signing failed", e);
+            log.error("HMAC_ALGO 관련 알 수 없는 에러발생 : {}", e.getMessage());
+            return null;
         }
     }
 
     public boolean validValue(String uuid, String signature) {
 
-        if(uuid == null || signature == null){return false;}
+        if (uuid == null || signature == null) {
+            return false;
+        }
         String expectedSig = createSignature(uuid);
         return expectedSig.equals(signature);
     }
