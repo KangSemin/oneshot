1574f3c0298f66f0240e7c600dea8ef665f54569
lee sujin <jin18302@naver.com>
2025-06-29 21:26:22 +0900

    feat : 인기 칵테일 초기 셋팅 값 설정 메서드 추가

--- src/main/java/salute/oneshot/domain/cocktail/repository/CocktailRepository.java
+++ src/main/java/salute/oneshot/domain/cocktail/repository/CocktailRepository.java
@@ -2,8 +2,10 @@ package salute.oneshot.domain.cocktail.repository;
 
 
 import io.lettuce.core.dynamic.annotation.Param;
+
 import java.util.List;
 import java.util.Optional;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,44 +13,47 @@ import org.springframework.data.jpa.repository.Query;
 import salute.oneshot.domain.cocktail.entity.Cocktail;
 import salute.oneshot.domain.cocktail.entity.RecipeType;
 
-public interface CocktailRepository extends JpaRepository<Cocktail, Long> , CocktailQueryDslRepository{
+public interface CocktailRepository extends JpaRepository<Cocktail, Long>, CocktailQueryDslRepository {
 
     @Query("""
-        SELECT DISTINCT c FROM Cocktail c
-        LEFT JOIN FETCH c.user
-        LEFT JOIN FETCH c.ingredientList ci
-        LEFT JOIN FETCH ci.ingredient
-        WHERE c.id = :id
-        """)
+            SELECT DISTINCT c FROM Cocktail c
+            LEFT JOIN FETCH c.user
+            LEFT JOIN FETCH c.ingredientList ci
+            LEFT JOIN FETCH ci.ingredient
+            WHERE c.id = :id
+            """)
     Optional<Cocktail> findById(@Param("id") Long id);
 
     boolean existsByIdAndUserId(Long cocktailId, Long userId);
 
     @Query("""
-        SELECT DISTINCT c FROM Cocktail c join fetch c.ingredientList l
-        join fetch l.ingredient
-        join fetch c.user
-        WHERE (:type IS NULL OR c.type = :type)
-        AND (:keyword IS NULL OR c.name LIKE CONCAT('%', :keyword ,'%') )
-        """)
+            SELECT DISTINCT c FROM Cocktail c join fetch c.ingredientList l
+            join fetch l.ingredient
+            join fetch c.user
+            WHERE (:type IS NULL OR c.type = :type)
+            AND (:keyword IS NULL OR c.name LIKE CONCAT('%', :keyword ,'%') )
+            """)
     Page<Cocktail> findCocktails(Pageable pageable, @Param("keyword") String keyword, @Param("type") RecipeType type);
 
     @Query("""
-        SELECT c FROM Cocktail c
-        LEFT JOIN FETCH c.user
-        LEFT JOIN FETCH c.ingredientList ci
-        LEFT JOIN FETCH ci.ingredient
-        WHERE c.id in :ids
-        """)
+            SELECT c FROM Cocktail c
+            LEFT JOIN FETCH c.user
+            LEFT JOIN FETCH c.ingredientList ci
+            LEFT JOIN FETCH ci.ingredient
+            WHERE c.id in :ids
+            """)
     Page<Cocktail> findAllById(List<Long> ids, Pageable pageable);
 
 
     @Query("""
-    SELECT c FROM Cocktail c
-    LEFT JOIN FETCH c.user
-    LEFT JOIN FETCH c.ingredientList ci
-    LEFT JOIN FETCH ci.ingredient
-    WHERE c.id in :ids
-    """)
+            SELECT c FROM Cocktail c
+            LEFT JOIN FETCH c.user
+            LEFT JOIN FETCH c.ingredientList ci
+            LEFT JOIN FETCH ci.ingredient
+            WHERE c.id in :ids
+            """)
     List<Cocktail> findAllById(List<Long> ids);
+
+    @Query(" SELECT c FROM Cocktail c WHERE true ORDER BY c.favoriteCount desc , c.viewCount desc LIMIT 10")
+    List<Cocktail> findTopN();
 }
